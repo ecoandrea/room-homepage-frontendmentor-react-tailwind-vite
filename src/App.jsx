ade14a73bff0085e8e7e1de394314945e4f3d2c2
Cristina Zamora <ecoandrea@hotmail.com>
2022-09-19 22:13:51 -0300

    dia 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,40 @@
 import ImgHero1 from "./assets/images/desktop-image-hero-1.jpg";
 import ImgAboutDark from "./assets/images/image-about-dark.jpg";
 import ImgAboutLight from "./assets/images/image-about-light.jpg";
-import IconArrowShop from "../components/IconArrowShop";
-import IconPrev from "../components/IconPrev";
-import IconNext from "../components/IconNext";
+import IconArrowShop from "./components/icon/IconArrowShop";
+import IconPrev from "./components/icon/IconPrev";
+import IconNext from "./components/icon/IconNext";
+import NavBar from "./components/menu/NavBar";
+
 const App = () => {
   return (
-    <div>
-      <nav>
-        <a href="#">room</a>
-        <div>
-          <a href="#">home</a>
-          <a href="#">shop</a>
-          <a href="#">about</a>
-          <a href="#">contact</a>
-        </div>
-      </nav>
-
-      <header className="grid grid-cols-1 lg:grid-cols-7  lg:grid-rows-[10fr_1.5fr]">
+    <div className="">
+      <NavBar />
+      <header className="grid grid-cols-3 grid-rows-[10fr_1.5fr_10fr] lg:grid-cols-7 lg:grid-rows-[10fr_1.5fr]">
         <img
           src={ImgHero1}
           alt=""
-          className="lg:col-span-4 lg:row-span-2 w-full h-full"
+          className="col-start-1 col-end-4 row-start-1 row-end-3 h-full w-full lg:col-start-1 lg:col-end-5 lg:row-start-1 lg:row-end-3"
         />
-        <article className="lg:col-span-3  lg:px-24 flex  flex-col justify-center items-start">
-          <h1 className="font-bold text-4xl mb-6">
+        <article className="col-start-1 col-end-4 row-start-3 row-end-4 flex flex-col items-start justify-center p-8 lg:col-start-5 lg:col-end-8 lg:row-start-1 lg:row-end-2 lg:px-16">
+          <h1 className="mb-6 text-4xl font-bold">
             Discover innovative ways to decorate
           </h1>
-          <p className="text-gray-500">
+          <p className="mb-8 text-gray-500">
             We provide unmatched quality, comfort, and style for property owners
             across the country. Our experts combine form and function in
             bringing your vision to life. Create a room in your own style with
             our collection and make your property a reflection of you and what
             you love.
           </p>
-          <button className="flex items-center hover:text-gray-500 hover:fill-gray-500 fill-black ">
-            <span className="font-bold text-lg uppercase tracking-[1rem]text-gray-500 ">
-              Shop Now
+          <button className="flex items-center fill-black hover:fill-gray-500 hover:text-gray-500">
+            <span className="text-base font-bold uppercase tracking-[1rem]">
+              Shop now
             </span>
             <IconArrowShop className="" />
           </button>
         </article>
-
-        <div className="bg-black text-white flex ">
+        <div className="col-start-3 col-end-4 row-start-2 row-end-3 flex bg-black text-white lg:col-start-5 lg:col-end-6 lg:row-start-2 lg:row-end-3">
           <button className="w-full">
             <IconPrev className="mx-auto" />
           </button>
@@ -55,11 +47,10 @@ const App = () => {
         <img
           src={ImgAboutDark}
           alt=""
-          className="lg:col-span-2 w-full h-full"
+          className="h-full w-full lg:col-span-2"
         />
-        <section className="lg:col-span-3 lg:px-16 flex flex-col justify-center">
-          <h2 className="font-bold text-lg uppercase tracking-[0.5rem] mb-4">
-            {" "}
+        <section className="flex flex-col justify-center p-8 lg:col-span-3 lg:px-16">
+          <h2 className="mb-6 text-lg font-bold uppercase tracking-[0.5rem]">
             About our furniture
           </h2>
           <p className="text-gray-500">
@@ -71,7 +62,11 @@ const App = () => {
             help you create your dream space.
           </p>
         </section>
-        <img src={ImgAboutLight} alt="" className="lg:col-span-2" />
+        <img
+          src={ImgAboutLight}
+          alt=""
+          className="h-full w-full lg:col-span-2"
+        />
       </main>
     </div>
   );
